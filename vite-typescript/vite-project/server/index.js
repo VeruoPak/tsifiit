a015a1bdd5544b2ac58f86276921ca47b78433bd
VeruoPak <veroupak@gmail.com>
2025-05-13 16:59:39 +0900

    add lab9

--- vite-typescript/vite-project/server/index.js
+++ vite-typescript/vite-project/server/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import cors from "cors";
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
+let dataSource = [];
+
+app.post("/data", (req, res) => {
+  const newData = req.body;
+  dataSource.push(newData);
+
+  res.status(201).json(newData);
+});
 
 app.get("/api/data", (req, res) => {
   res.json({ title: "Хлеб ржаной", descriptipon: "Хлебобулочное изделие", cost: "100" });
